a884b43c53a9e28906c82f931e63d91dc1ab3b1f
Luis Miguel Miranda <lmirandad27@gmail.com>
2023-01-31 10:26:52 -0800

    First Commit

---
+++ ScreenSeekers/src/main/java/com/webwizards/screenseekers/ScreenSeekersApplication.java
@@ -0,0 +1,13 @@
+package com.webwizards.screenseekers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenSeekersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenSeekersApplication.class, args);
+	}
+
+}
---
+++ ScreenSeekers/src/test/java/com/webwizards/screenseekers/ScreenSeekersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webwizards.screenseekers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenSeekersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
