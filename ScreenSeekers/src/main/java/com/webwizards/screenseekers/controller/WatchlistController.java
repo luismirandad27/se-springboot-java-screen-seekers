5acef9bccc3b04ed0f6a3974879e36f5f1f8f546
Marc <user@DESKTOP-UTA561D.vf.shawcable.net>
2023-03-01 21:13:47 -0800

    initial commit

--- ScreenSeekers/src/main/java/com/webwizards/screenseekers/controller/WatchlistController.java
+++ ScreenSeekers/src/main/java/com/webwizards/screenseekers/controller/WatchlistController.java
@@ -20,7 +20,7 @@ package com.webwizards.screenseekers.controller;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
+import java.util.Optional; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
